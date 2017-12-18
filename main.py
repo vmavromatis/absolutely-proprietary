c017d908d44f56b747b20580dfb5426e23c7bbfb
Vasilis Mavromatis <vmavromatis@hotelbeds.com>
2017-12-18 00:24:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import sys
 import subprocess
 import urllib2
@@ -16,15 +17,20 @@ f2 = open("blacklist.txt", "w+")
 f2.write(urllib2.urlopen('https://git.parabola.nu/blacklist.git/plain/blacklist.txt').read())
 f2.close()
 
-#TODO compare lists
-#
-#
-#
-#
-#
-#
-
+#FIXME comparison not working well - Compare lists
+countproprietary=0
+with open('installed.txt') as installed:
+    for package in installed:
+        with open("blacklist.txt") as f:
+            for line in f:
+                if package in line:
+                     print (line)
+                     countproprietary +=1
 
 #Print results
-cprint(figlet_format('ABSOLUTELY PROPRIETARY!', font='univers', width=160),
+cprint(figlet_format(('%s ABSOLUTELY PROPRIETARY PACKAGES' % (countproprietary)), font='univers', width=160),
        'green', attrs=['bold'])
+
+total=int(subprocess.check_output(['bash', '-c', 'pacman -Q | wc -l', 'shell=True']))
+stallmanfreedomindex=(total-countproprietary)*100/total
+print ("%s proprietary out of %s total installed. Your Stallman Freedom index is %.2f") % (countproprietary,total,stallmanfreedomindex)
