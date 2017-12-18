209079f738ba93c5d0135c51b1cfcc6ec01d606e
Vasilis Mavromatis <8668731+vmavromatis@users.noreply.github.com>
2017-12-18 19:18:42 +0000

    added savefile

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ f2 = open("blacklist.txt", "w+")
 f2.write(urllib2.urlopen('https://git.parabola.nu/blacklist.git/plain/blacklist.txt').read())
 f2.close()
 
-#FIXME comparison not working well - Compare lists
+#Generate proprietary list
+f3 = open("disgusting.txt", "w+")
+
+#Compare lists
 countproprietary=0
 with open('installed.txt') as installed:
     for package in installed:
@@ -26,11 +29,13 @@ with open('installed.txt') as installed:
             for line in f:
                 # Find package names in blacklist
                 real_pkg_name = line.split(':')[0]
+                reason = ((line.split(':')[4]).split(']')[0]+"]").strip()
 
                 if real_pkg_name == package:
-                    print (line)
+                    f3.write(package+" > "+reason+"\n")
                     countproprietary +=1
                     break
+f3.close()
 
 #Print results
 cprint(figlet_format(('%s ABSOLUTELY PROPRIETARY PACKAGES' % (countproprietary)), font='univers', width=160),
@@ -38,4 +43,5 @@ cprint(figlet_format(('%s ABSOLUTELY PROPRIETARY PACKAGES' % (countproprietary))
 
 total=int(subprocess.check_output(['bash', '-c', 'pacman -Q | wc -l', 'shell=True']))
 stallmanfreedomindex=(total-countproprietary)*100/total
-print ("%s proprietary out of %s total installed. Your Stallman Freedom index is %.2f") % (countproprietary,total,stallmanfreedomindex)
+print("Your GNU/Linux is infected with %s proprietary packages out of %s total installed. Your Stallman Freedom Index is %.2f.\n") % (countproprietary,total,stallmanfreedomindex)
+print("The proprietary packages have been saved as disgusting.txt")
